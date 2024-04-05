24e7198cc9caeabfd2b1801b0be972045d234c11
Borja Muñoz <borja.munoz@gmail.com>
2024-04-06 01:28:12 +0200

    Add sidebar

--- app/src/components/DefaultView.tsx
+++ app/src/components/DefaultView.tsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom';
-
 import Header from './Header';
 import ModalMessage from './ModalMessage';
+import Sidebar from './Sidebar';
+import { Grid } from '@mui/material';
 
 export default function DefaultView() {
   return (
     <>
       <Header />
-      <Outlet />
+      <Grid container>
+        <Grid item xs={3}>
+          <Sidebar />
+        </Grid>
+        <Grid item xs={9}>
+          <Outlet />
+        </Grid>
+      </Grid>
       <ModalMessage />
     </>
   );
---
+++ app/src/components/Sidebar.tsx
@@ -0,0 +1,39 @@
+import { styled } from '@mui/material/styles';
+import { Box, Theme } from '@mui/material';
+import { Drawer } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+export const DRAWER_WIDTH = 250;
+
+const NavDrawer = styled('nav')(({ theme }: { theme: Theme }) => ({
+  flex: '0 0 auto',
+  position: 'relative',
+  width: DRAWER_WIDTH,
+  flexShrink: 0,
+}));
+
+const DrawerDesktop = styled(Drawer)(() => ({
+  '& .MuiPaper-root': {
+    width: DRAWER_WIDTH,
+    position: 'absolute',
+    height: '100vh',
+    padding: '20px',
+    borderWidth: '0px',
+  },
+}));
+
+export default function Sidebar() {
+
+  return (
+    <NavDrawer>
+      <DrawerDesktop variant='permanent' anchor='left' open>
+          <Box mb={1}>
+            <Link to="/">Home</Link>
+          </Box>
+          <Box>
+            <Link to="/dashboard">Dashboard</Link>
+          </Box>
+      </DrawerDesktop>
+    </NavDrawer>
+  );
+}
