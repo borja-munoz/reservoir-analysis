b62d272dce891bc8efb224ce6e3036350a4055cd
Borja Muñoz <borja.munoz@gmail.com>
2024-04-06 00:24:25 +0200

    Add functions for getting a connection and executing a query

--- app/src/db/duckdb.ts
+++ app/src/db/duckdb.ts
@@ -5,32 +5,60 @@ import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
 import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
 
 import type { AsyncDuckDB } from '@duckdb/duckdb-wasm';
+import { DuckDBAccessMode, type AsyncDuckDBConnection } from "@duckdb/duckdb-wasm";
 
 const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
-    mvp: {
-        mainModule: duckdb_wasm,
-        mainWorker: mvp_worker,
-    },
-    eh: {
-        mainModule: duckdb_wasm_eh,
-        mainWorker: eh_worker,
-    },
+  mvp: {
+    mainModule: duckdb_wasm,
+    mainWorker: mvp_worker,
+  },
+  eh: {
+    mainModule: duckdb_wasm_eh,
+    mainWorker: eh_worker,
+  },
 };
 
 let db : AsyncDuckDB | null = null;
+let connection: Promise<AsyncDuckDBConnection> | null; // Declare globally so promise can be awaited anywhere
 
 const initDB = async () => {
-    if (db) {
-        return db; // Return existing database, if any
-    }
-    // Select a bundle based on browser checks
-    const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
-    // Instantiate the asynchronus version of DuckDB-wasm
-    const worker = new Worker(bundle.mainWorker!);
-    const logger = new duckdb.ConsoleLogger();
-    db = new duckdb.AsyncDuckDB(logger, worker);
-    await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
-    return db;
+  if (db) {
+    return db; // Return existing database, if any
+  }
+
+  // Select a bundle based on browser checks
+  const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+
+  // Instantiate the asynchronus version of DuckDB-wasm
+  const worker = new Worker(bundle.mainWorker!);
+  const logger = new duckdb.ConsoleLogger();
+  db = new duckdb.AsyncDuckDB(logger, worker);
+  await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+
+  return db;
 }; 
 
-export { initDB };
+const getConnection = async () => {
+  if (connection) {
+    return connection; // Return existing promise, if any
+  }
+
+  // Initialize database
+  const db = await initDB();
+
+  await db.open({ 
+    accessMode: DuckDBAccessMode.READ_ONLY,
+    path: "http://localhost:5173/db/reservoir-analysis.db"
+  });
+
+  connection = db.connect(); // Open a connection (promise)
+  return connection;
+};
+
+const executeQuery = async (query: string) => {
+  const conn = await getConnection();
+  const results = await conn?.query(query);
+  return results;
+}
+
+export { executeQuery };
