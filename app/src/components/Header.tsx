399c3a378b292cc8ecfaaa5daf1b4e82bfd911c9
Borja Muñoz <borja.munoz@gmail.com>
2024-04-06 11:49:01 +0200

    Switch to app bar with navigation links

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useLocation } from 'react-router-dom';
 
-import Grid from "@mui/material/Grid";
+import { AppBar, Container, Grid, Tab, Tabs, Toolbar } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { Box, FormControl, Typography } from "@mui/material";
 import FlareOutlinedIcon from "@mui/icons-material/FlareOutlined";
 
+import { ROUTE_PATHS } from '../routes';
 import { LOCALES } from "../i18n/locales";
 import { FormattedMessage, useIntl } from "react-intl";
 import { RootState } from "../store/store";
@@ -19,11 +22,55 @@ const languages = [
   { name: "Spanish", code: LOCALES.SPANISH },
 ];
 
+const NavTabs = styled(Grid)(({ theme }) => ({
+  flexDirection: 'row',
+  alignContent: 'flex-start',
+
+  '& .MuiTab-root': {
+    color: theme.palette.common.white,
+
+    '&:hover': {
+      borderBottomColor: theme.palette.secondary.main,
+    },
+  },
+  '& .MuiTabs-indicator': {
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const VerticalNavTabs = styled(Grid)(({ theme }) => ({
+  flexDirection: 'column',
+
+  '& .MuiTabs-root': {
+    boxShadow: 'none',
+  },
+  '& .MuiTabs-vertical .MuiTab-root': {
+    justifyContent: 'flex-start',
+    paddingLeft: theme.spacing(2),
+  },
+}));
+
 export default function Header() {
   const dispatch = useDispatch();
   const locale = useSelector((state: RootState) => state.app.locale);
   const [language, setLanguage] = useState(locale);
   const intl = useIntl();
+  const location = useLocation();
+  const pathname = location.pathname.split('/')[1] || false;
+  const vertical = false;
+
+  const WrapperTabs = ({
+    vertical = false,
+    children,
+  }: {
+    vertical?: boolean;
+    children: React.ReactNode;
+  }) =>
+    vertical ? (
+      <VerticalNavTabs container>{children}</VerticalNavTabs>
+    ) : (
+      <NavTabs container sx={{width: '50%'}}>{children}</NavTabs>
+    );
 
   const handleChange = (
     event: SelectChangeEvent<string>,
@@ -34,38 +81,69 @@ export default function Header() {
   };
 
   return (
-    <Grid
-      container
-      spacing={1}
-      mt={1}
-      ml={1}
-      sx={{ minHeight: "50px", maxHeight: "100px", marginBottom: "10px" }}
-    >
-      <Grid item xs={8}>
-        <FlareOutlinedIcon color="primary" sx={{ mr: 2 }} fontSize="large" />
-        <Typography color="primary" alignItems="top" component="span" variant="h5">Reservoir Analysis</Typography>
-      </Grid>
-      <Grid item xs={4}>
-        <Box display="flex" justifyContent="flex-end" mr={4}>
-          <FormControl size="small">
-            <InputLabel id="language-label">
-              <FormattedMessage id="language" />
-            </InputLabel>
-            <Select
-              labelId="language-label"
-              label={intl.formatMessage({ id: "language" })}
-              value={language}
-              onChange={handleChange}
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <FlareOutlinedIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 2 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="span"
+            sx={{
+              minWidth: '275px',
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Reservoir Analysis
+          </Typography>
+          <WrapperTabs vertical={vertical}>
+            <Tabs
+              value={pathname ? pathname : ''}
+              textColor={vertical ? 'primary' : 'inherit'}
+              orientation={vertical ? 'vertical' : 'horizontal'}
             >
-              {languages.map(({ name, code }) => (
-                <MenuItem key={code} value={code}>
-                  {name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-      </Grid>
-    </Grid>
+              <Tab
+                label='Home'
+                value=''
+                component={NavLink as any}
+                to={ROUTE_PATHS.DEFAULT}
+              />
+              <Tab
+                label='Dashboard'
+                value='dashboard'
+                component={NavLink as any}
+                to={ROUTE_PATHS.DASHBOARD}
+              />
+            </Tabs>
+          </WrapperTabs>        
+          <Box display="flex" justifyContent="flex-end">
+            <FormControl size="small">
+              <InputLabel id="language-label" sx={{color: "#FFFFFF"}}>
+                <FormattedMessage id="language" />
+              </InputLabel>
+              <Select
+                labelId="language-label"
+                label={intl.formatMessage({ id: "language" })}
+                value={language}
+                onChange={handleChange}
+                sx={{color: "#FFFFFF", borderColor: "#FFFFFF"}}
+              >
+                {languages.map(({ name, code }) => (
+                  <MenuItem key={code} value={code}>
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
