3e2046734bac6aebeb38fc0c6f9b79cab7ed1cfb
Borja Muñoz <borja.munoz@gmail.com>
2024-04-07 11:46:03 +0200

    Remove strict mode to avoid downloading the wasm bundle twice

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux";
 import store from "./store/store";
@@ -7,11 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
