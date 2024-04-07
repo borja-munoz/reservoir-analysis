81c29a3241e3c01d08af0486e5361812c89a2481
Borja Muñoz <borja.munoz@gmail.com>
2024-04-08 00:06:56 +0200

    Switch to RichTreeView

--- app/src/components/EntitySelector.tsx
+++ app/src/components/EntitySelector.tsx
@@ -1,8 +1,8 @@
-import { ReactElement, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Drawer, Theme } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { SimpleTreeView, TreeItem } from '@mui/x-tree-view';
+import { RichTreeView, TreeViewBaseItem } from "@mui/x-tree-view";
 
 import { executeQuery } from "../db/duckdb";
 import { useDispatch, useSelector } from "react-redux";
@@ -11,9 +11,16 @@ import { setSelectedEntity } from "../store/appSlice";
 
 export const DRAWER_WIDTH = 350;
 
+export enum EntityType {
+  Basin = "Basin",
+  Province = "Province",
+  Station = "Station",
+}
+
 export type SelectedEntity = {
-  type: 'basin' | 'province' | 'station';
-  name: string;
+  type: EntityType;
+  id: string;
+  idBasin: number;
 };
 
 const NavDrawer = styled("nav")(({ theme }: { theme: Theme }) => ({
@@ -35,9 +42,11 @@ const DrawerDesktop = styled(Drawer)(() => ({
 
 export default function EntitySelector() {
   const dispatch = useDispatch();
-  const dbInitialized = useSelector((state: RootState) => state.app.dbInitialized);
+  const dbInitialized = useSelector(
+    (state: RootState) => state.app.dbInitialized
+  );
   const [data, setData] = useState<any[] | undefined>();
-  const treeItems: ReactElement[]  = [];
+  let treeItems: TreeViewBaseItem[] = [];
 
   useEffect(() => {
     const fetchData = async () => {
@@ -51,65 +60,88 @@ export default function EntitySelector() {
       const arrowTable = await executeQuery(query);
       setData(arrowTable?.toArray());
     };
-  
+
     if (dbInitialized) {
       fetchData();
     }
   }, [dbInitialized]);
 
+  const createTreeItems = () => {
+    if (data !== undefined) {
+      treeItems.push({
+        id: "basin|1",
+        label: "Hidrosur",
+      });
+      let currentIndexProvince = 0;
+      treeItems[0].children = [
+        {
+          id: `basin|1|province|${data[0].province}`,
+          label: data[0].province,
+          children: [],
+        },
+      ];
+      data.forEach((station, index) => {
+        if (
+          station.province !==
+          treeItems[0].children![currentIndexProvince].label
+        ) {
+          treeItems[0].children?.push({
+            id: `basin|1|province|${station.province}`,
+            label: station.province,
+            children: [],
+          });
+          currentIndexProvince += 1;
+        }
+        treeItems[0].children![currentIndexProvince].children!.push({
+          id: `basin|1|station|${station.station_id}`,
+          label: station.station_name,
+        });
+      });
+    }
+  };
+
   if (data !== undefined) {
-    let currentProvince = data[0].province;
-    let stationItems: ReactElement[]  = [];
-    data.forEach((station, index) => {
-      if (station.province !== currentProvince) {
-        treeItems.push(
-          <TreeItem 
-            key={index * -1} 
-            itemId={String(index * -1)} 
-            label={currentProvince}
-          >
-            {stationItems}
-          </TreeItem>
-        );  
-        currentProvince = station.province;
-        stationItems = [];
-      }
-      stationItems.push(
-        <TreeItem 
-          key={index} 
-          itemId={String(station.station_id)} 
-          label={station.station_name} 
-        />
-      );
-    });
+    createTreeItems();
   }
 
   const handleItemSelectionToggle = (
     event: React.SyntheticEvent,
     itemId: string,
-    isSelected: boolean,
+    isSelected: boolean
   ) => {
     if (isSelected) {
-      dispatch(setSelectedEntity({
-        type: 'basin',
-        name: itemId
-      }));
+      let itemElements = itemId.split("|");
+      let selectedEntity;
+      selectedEntity =
+        itemElements.length == 2
+          ? {
+              type: EntityType.Basin,
+              id: itemElements[1],
+              idBasin: parseInt(itemElements[1]),
+            }
+          : {
+              type:
+                itemElements[2] === "province"
+                  ? EntityType.Province
+                  : EntityType.Station,
+              id: itemElements[3],
+              idBasin: parseInt(itemElements[1]),
+            };
+      dispatch(setSelectedEntity(selectedEntity));
     }
   };
 
   return (
     <NavDrawer>
       <DrawerDesktop variant="permanent" anchor="left" open>
-        <SimpleTreeView
-          // expandedItems={['hidrosur']}
+        <RichTreeView
+          items={treeItems}
+          defaultSelectedItems={'basin|1'}
+          defaultExpandedItems={['basin|1']}
           aria-label="Entity Selector"
           sx={{ height: 200, flexGrow: 1, maxWidth: 350, overflowY: "auto" }}
           onItemSelectionToggle={handleItemSelectionToggle}
-        >
-          <TreeItem itemId="hidrosur" label="Hidrosur">
-            {treeItems}
-          </TreeItem>
-        </SimpleTreeView>
+        />
       </DrawerDesktop>
     </NavDrawer>
   );
