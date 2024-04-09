ea78c4cc4a6592f3f402d189b7fd6787b3febd4f
Borja Muñoz <borja.munoz@gmail.com>
2024-04-09 21:28:02 +0200

    Formatter

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 
-import App from './App.tsx'
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
-)
+);
