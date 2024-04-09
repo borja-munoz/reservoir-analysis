134d90b1162fd25170520d9f3725afdf890d428a
Borja Muñoz <borja.munoz@gmail.com>
2024-04-09 21:27:06 +0200

    Reduce database size by exporting and importing

--- reduce_database_size.py
+++ reduce_database_size.py
@@ -6,20 +6,34 @@ from datetime import datetime
 # Backup database
 now = datetime.now()
 dt_string = now.strftime("%Y%m%d_%H%M%S")
+print("Creating a database backup...")
 shutil.copy('db/reservoir-analysis.db', 'db/backup/reservoir-analysis_' + dt_string + '.db')
 
 # Export database
+print("Exporting a database dump...")
 con = duckdb.connect('db/reservoir-analysis.db') # has big file size
-con.execute("export database 'db/db-dump';")
+con.execute("EXPORT DATABASE 'db/db-dump';")
 con.close()
 
 # Remove database
+print("Removing existing database...")
 os.remove('db/reservoir-analysis.db')
 
-# Re-import database
+# Recreate database
+# In order to be able to load the spatial extension, it needs
+# to be pre-installed in the ~/.duckdb/extensions directory
+print("Creating a new database and loading the spatial extension...")
 con = duckdb.connect('db/reservoir-analysis.db')
-con.execute("import database 'db/db-dump';")
+con.execute("INSTALL spatial;")
+con.execute("LOAD spatial;")
+
+# Import data from the dump
+print("Importing from the dump files...")
+con.execute("IMPORT DATABASE 'db/db-dump';")
 con.close()
 
 # Delete dump
-shutil.rmtree(('db/db-dump')
+print("Deleting the dump files...")
+shutil.rmtree('db/db-dump')
+
+print("Process completed.")
