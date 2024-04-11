16190f3788eb2e8dd4782b3f41a77bfb2e5ed6d3
Borja Muñoz <borja.munoz@gmail.com>
2024-04-11 22:04:30 +0200

    Update measurement queries

--- create_summary_tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE TABLE basin_reservoir_yearly_average AS
-SELECT AVG(volume_hm3) AS volume_hm3,
-       EXTRACT('year' FROM hour) AS year
-  FROM measurements_reservoir
- GROUP BY year
- ORDER BY year;
