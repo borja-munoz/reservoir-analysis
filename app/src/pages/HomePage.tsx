e2a5c890fedd75566fd913e32f79500c4636786f
Borja Muñoz <borja.munoz@gmail.com>
2024-04-16 01:11:15 +0200

    Add link to dashboard

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { FormattedMessage } from "react-intl";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   
@@ -16,6 +17,9 @@ export default function HomePage() {
       <Typography variant="h2" color="primary">
         <FormattedMessage id="welcome" />
       </Typography>
+      <Box>
+        <Link to="/dashboard">Go to Dashboard</Link>
+      </Box>
     </Box>
   );
 }
