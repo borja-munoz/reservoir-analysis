6804995cb2a3e0082bb55ad70b3426d06efb6267
Borja Muñoz <borja.munoz@gmail.com>
2024-04-16 00:48:45 +0200

    Switch to HashRouter

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
