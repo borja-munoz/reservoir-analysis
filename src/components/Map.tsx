bc742bb1a9d7e124e8dd50e6ff2c2b5ead875a37
Borja Muñoz <borja.munoz@gmail.com>
2024-04-20 23:19:45 +0200

    Fit map extent to selected stations

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import DeckGL from "@deck.gl/react";
 import { Map as MapLibreMap } from "react-map-gl/maplibre";
 import { GeoJsonLayer } from "@deck.gl/layers";
+import { WebMercatorViewport } from "@deck.gl/core";
 import type { Feature } from "geojson";
 
 import "maplibre-gl/dist/maplibre-gl.css";
@@ -25,6 +26,12 @@ type StationProperties = {
 
 export default function Map({ stations }: { stations: any[] }) {
   const [data, setData] = useState<Feature[]>([]);
+  const [boundsFitted, setBoundsFitted] = useState(false);
+  const [initialViewState, setInitialViewState] = useState({
+    latitude: 37,
+    longitude: -4.5,
+    zoom: 6.3,
+  });
 
   // Transform stations data to GeoJSON
   useEffect(() => {
@@ -41,8 +48,40 @@ export default function Map({ stations }: { stations: any[] }) {
           },
         };
       })
-    );  
+    );
+    setBoundsFitted(false);
   }, [stations]);
+
+  const getLayerBounds = () => {
+    let minX = Number.MAX_VALUE;
+    let minY = Number.MAX_VALUE;
+    let maxX = - Number.MAX_VALUE;
+    let maxY = - Number.MAX_VALUE;
+
+    if (stations.length == 1) {
+      minX = maxX = stations[0].longitude;
+      minY = maxY = stations[0].latitude;
+    } else {
+      stations.forEach((station) => {
+        if (station.longitude !== null) {
+          if (station.longitude < minX) {
+            minX = station.longitude;
+          }
+          if (station.longitude > maxX) {
+            maxX = station.longitude;
+          }
+          if (station.latitude < minY) {
+            minY = station.latitude;
+          }
+          if (station.latitude > maxY) {
+            maxY = station.latitude;
+          }  
+        }
+      });
+    }
+
+    return [[minX, minY], [maxX, maxY]];
+  };
 
   const layer = new GeoJsonLayer<StationProperties>({
     id: "GeoJsonLayer",
@@ -55,16 +94,32 @@ export default function Map({ stations }: { stations: any[] }) {
     pointRadiusUnits: "pixels",
   });
 
+  const onAfterRender = () => {
+    if (!boundsFitted) {
+      //@ts-ignore
+      if (layer.props.data.length > 0) {
+        const viewport = layer.context.viewport as WebMercatorViewport;
+        const { longitude, latitude, zoom } = viewport.fitBounds(
+          //@ts-ignore
+          getLayerBounds(), 
+          {
+            minExtent: 0.1, // degrees
+            padding: 50,    // pixels
+          }
+        );
+        setInitialViewState({ longitude, latitude, zoom });
+        setBoundsFitted(true);
+      }
+    }
+  };
+
   return (
     <MapContainer>
       <DeckGL
-        initialViewState={{
-          latitude: 37,
-          longitude: -4.5,
-          zoom: 6.3,
-        }}
+        initialViewState={initialViewState}
         controller={true}
         layers={[layer]}
+        onAfterRender={onAfterRender}
       >
         <MapLibreMap reuseMaps mapStyle={MAP_STYLE} styleDiffing={false} />
       </DeckGL>
